2d8b0e0b3a3c7341214fe1d6e3d6da1360c26402
Karkachevich <karkachevichtv@yandex.ru>
2021-12-05 18:15:19 +0500

    feat: добавить 6 карточек и актевировать like

--- index.js
+++ index.js
@@ -33,3 +33,45 @@ function formSubmitHandler (evt) {
 
 formPopupWinow.addEventListener('submit', formSubmitHandler);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
+
+  const elementsList = document.querySelector('.elements');
+  const elementTemplate = document.querySelector('#element').content;
+
+  initialCards.forEach(function (elm) {
+
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+    element.querySelector('.element__image').src = elm.link;
+    element.querySelector('.element__title').textContent = elm.name;
+    element.querySelector('.element__like').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('element__like_active');
+    })
+
+    elementsList.append(element);
+  });
+
