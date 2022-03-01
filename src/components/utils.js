1241d189a7f588478e146111774f9a63a0fdc88b
Karkachevich <karkachevichtv@yandex.ru>
2022-03-01 15:44:37 +0500

    feat: взаимодействие с сервером

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,30 +1,38 @@
+/*
 export const initialCards = [
   {
     name: "Архыз",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    like: 0,
   },
   {
     name: "Челябинская область",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    like: 0,
   },
   {
     name: "Иваново",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    like: 0,
   },
 
   {
     name: "Камчатка",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    like: 0,
   },
   {
     name: "Холмогорский район",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    like: 0,
   },
   {
     name: "Байкал",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    like: 0,
   },
 ];
+*/
 
 export const validationConfig = {
   form: ".form",
