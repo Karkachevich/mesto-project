5c7cd74785f6b9d587d651f80301921f112ab7a6
Karkachevich <karkachevichtv@yandex.ru>
2022-04-03 15:03:53 +0500

    feat: добавил class PopupWitnDelete

---
+++ src/components/PopupWithDelete.js
@@ -0,0 +1,26 @@
+import Popup from "./Popup";
+
+export default class PopupWithDelete extends Popup {
+  constructor({ selector, handleCardDelete }) {
+    super(selector);
+    this._elementFormDelete = this._popup.querySelector(".form_type_delete");
+    this._handleCardDelete = handleCardDelete;
+  }
+
+  setEventListeners() {
+    super._setEventListeners();
+    this._elementFormDelete.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleCardDelete(this._element);
+    });
+  }
+
+  open(element) {
+    this._element = element;
+    super.open();
+  }
+
+  close() {
+    super.close();
+  }
+}
