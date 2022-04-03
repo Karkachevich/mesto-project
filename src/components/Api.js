87da7606b8cfd212eca35899540eb04926e72853
Karkachevich <karkachevichtv@yandex.ru>
2022-04-03 15:56:51 +0500

    feat: собрал в index

--- src/components/Api.js
+++ src/components/Api.js
@@ -57,8 +57,8 @@ export default class Api {
     }).then(this._checkResponse);
   }
 
-  deleteMyCard(cardId) {
-    return fetch(`${this._url}/cards/${cardId}`, {
+  deleteMyCard(id) {
+    return fetch(`${this._url}/cards/${id}`, {
       method: "DELETE",
       headers: this._headers
     }).then(this._checkResponse);
@@ -79,95 +79,3 @@ export default class Api {
   }
 }
 
-
-/////////
-const config = {
-  url: "https://nomoreparties.co/v1/plus-cohort7",
-  hearders: {
-    authorization: "606c19ec-1c82-4e12-b9a2-b32d3b016e4d",
-    "Content-Type": "application/json"
-  }
-};
-
-const checkResponse = (res) => {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(new Error(`Ошибка: ${res.status}`));
-};
-
-const getCards = () => {
-  return fetch(`${config.url}/cards`, {
-    headers: config.hearders
-  }).then(checkResponse);
-};
-
-const getProfile = () => {
-  return fetch(`${config.url}/users/me`, {
-    headers: config.hearders,
-  }).then((res) => checkResponse(res));
-};
-
-const editProfile = (profile) => {
-  return fetch(`${config.url}/users/me`, {
-    method: "PATCH",
-    headers: config.hearders,
-    body: JSON.stringify({
-      name: profile.name,
-      about: profile.about
-    })
-  }).then(checkResponse);
-};
-
-const editAvatar = (link) => {
-   return fetch(`${config.url}/users/me/avatar`, {
-    method: "PATCH",
-    headers: config.hearders,
-    body: JSON.stringify({
-      avatar: link.value
-    }),
-  }).then(checkResponse);
-};
-
-const settingLike = (id) => {
-  return fetch(`${config.url}/cards/likes/${id}`, {
-    method: "PUT",
-    headers: config.hearders
-  }).then(checkResponse);
-};
-
-const removeLike = (id) => {
-  return fetch(`${config.url}/cards/likes/${id}`, {
-    method: "DELETE",
-    headers: config.hearders
-  }).then(checkResponse);
-};
-
-const deleteCard = (element) => {
-  return fetch(`${config.url}/cards/${element.id}`, {
-    method: "DELETE",
-    headers: config.hearders
-  }).then(checkResponse);
-};
-
-const createNewCardSubmit = (card) => {
-  return fetch(`${config.url}/cards`, {
-    method: "POST",
-    headers: config.hearders,
-    body: JSON.stringify({
-      name: card.name,
-      link: card.link
-    }),
-  }).then(checkResponse);
-};
-
-export {
-  getCards,
-  getProfile,
-  editProfile,
-  createNewCardSubmit,
-  settingLike,
-  removeLike,
-  deleteCard,
-  editAvatar,
-};
--- src/components/index.js
+++ src/components/index.js
@@ -1,65 +1,40 @@
 import "../index.css";
-import { openPopup, closePopup } from "./modal.js";
-import { createCard } from "./CardClass.js";
-import { renderLoading } from "./utils.js";
+
 import { validationConfig, configApi } from "./constants";
-import { FormValidator } from "./FormValidator.js";
-import { UserInfo } from "./UserInfo.js";
-import { Popup } from "./Popup.js";
-import { PopupWithForm } from "./PopupWithForm.js";
-import { PopupWithImage } from "./PopupWithImage.js";
-import { PopupWithDelete } from "./PopupWithDelete.js";
-import {
-  getCards,
-  getProfile,
-  editProfile,
-  editAvatar,
-  deleteCard,
-  createNewCardSubmit,
-} from "./ApiClass";
+import FormValidator from "./FormValidator.js";
+import Card from "./Card";
+import Section from "./Section";
+import UserInfo from "./UserInfo.js";
+import PopupWithForm from "./PopupWithForm.js";
+import PopupWithImage from "./PopupWithImage.js";
+import PopupWithDelete from "./PopupWithDelete.js";
+import Api from "./Api.js";
 
-const popups = document.querySelectorAll(".popup");
 
-const popupEdit = document.querySelector(".popup_type_profile");
-const popupAdd = document.querySelector(".popup_type_card-add");
-const popupAvatar = document.querySelector(".popup_type_avatar");
 const editButton = document.querySelector(".profile__button-edit");
 const addButton = document.querySelector(".profile__button-add");
 const avatarButton = document.querySelector(".profile__avatar-cover");
 
 const formProfile = document.querySelector(".form_type_profile");
 const nameProfileInput = formProfile.querySelector(".form__input_type_name");
-const hobbyProfileInput = formProfile.querySelector(".form__input_type_hobby");
-const formButtonProfile = formProfile.querySelector(".form__button_type_profile");
 
 const formCard = document.querySelector(".form_type_card");
-const nameCardInput = formCard.querySelector(".form__input_type_title");
-const linkCardInput = formCard.querySelector(".form__input_type_link");
-const formButtonCard = formCard.querySelector(".form__button_type_card");
 
 const avatar = document.querySelector(".profile__avatar-pucture");
 const formAvatar = document.querySelector(".form_type_avatar");
-const linkAvatarInput = formAvatar.querySelector(".form__input_type_avatar");
-const formButtonAvatar = formAvatar.querySelector(".form__button_type_avatar");
 
-const nameProfile = document.querySelector(".profile__name");
-const hobbyProfile = document.querySelector(".profile__hobby");
-
-const formDelete = document.querySelector(".form_type_delete");
-const elementsList = document.querySelector(".elements");
-/////////////////////////////////////////////////////////////////////////////////
 
 const user = new UserInfo({ }, ".profile__name", ".profile__hobby");
 
-const api = new Api(config);
+const api = new Api(configApi);
+
+console.log(configApi)
 
 const popupImage = new PopupWithImage(".popup_type_picture");
 
 const popupDelete = new PopupWithDelete({
   selector: ".popup_type_delete",
   handleCardDelete: ({ element, cardId }) => {
-    //const elementAct = document.querySelector(".element_active");
-    //console.log(cardId)
     api
       .deleteMyCard(cardId)
       .then(() => {
@@ -217,139 +192,3 @@ Promise.all([api.getInitialProfile(), api.getInitialCards()])
     console.log("Ошибка загрузки данных", err.message);
   });
 
-///////////////////////////////////////////////////////////////////////////////////
-
-
-
-enableValidation(validationConfig);
-
-Promise.all([getProfile(), getCards()])
-  .then(([userData, cards]) => {
-    user.id = userData._id;
-    nameProfile.textContent = userData.name;
-    hobbyProfile.textContent = userData.about;
-    avatar.src = userData.avatar;
-
-    cards.forEach(function (item) {
-      const isMyLike = item.likes.filter((item) => item._id === user.id);
-      const isMyCard = item.owner._id === user.id;
-      elementsList.append(createCard(item, isMyLike.length > 0, isMyCard));
-    });
-  })
-  .catch((err) => {
-    console.log("Ошибка загрузки данных", err.message);
-  });
-
-
-avatarButton.addEventListener("click", function () {
-  enableValidation(validationConfig);
-   openPopup(popupAvatar);
-});
-
-editButton.addEventListener("click", function () {
-  nameProfileInput.value = nameProfile.textContent;
-  hobbyProfileInput.value = hobbyProfile.textContent;
-  enableValidation(validationConfig);
-  openPopup(popupEdit);
-});
-
-addButton.addEventListener("click", function () {
-  enableValidation(validationConfig);
-  openPopup(popupAdd);
-});
-
-popups.forEach((elm) => {
-  elm.addEventListener("mousedown", function (e) {
-    if (
-      e.target.classList.contains("popup") ||
-      e.target.classList.contains("popup__close")
-    ) {
-      closePopup(elm);
-    }
-  });
-});
-
-function handleProfileSubmit(evt) {
-  evt.preventDefault();
-  renderLoading(true, formButtonProfile);
-  const profile = {
-    name: nameProfileInput.value,
-    about: hobbyProfileInput.value,
-  };
-
-  editProfile(profile)
-    .then((res) => {
-      nameProfile.textContent = res.name;
-      hobbyProfile.textContent = res.about;
-      closePopup(popupEdit);
-    })
-    .catch((err) => {
-      console.log("Ошибка изменения профиля", err.message);
-    })
-    .finally(() => {
-      renderLoading(false, formButtonProfile );
-    });
-}
-
-function handleAvatarSubmit(evt) {
-  evt.preventDefault();
-  renderLoading(true, formButtonAvatar);
-  editAvatar(linkAvatarInput)
-    .then((res) => {
-      avatar.src = res.avatar;
-      closePopup(popupAvatar);
-    })
-    .catch((err) => {
-      console.log("Ошибка изменения аватара", err.message);
-    })
-    .finally(() => {
-      renderLoading(false, formButtonAvatar);
-    });
-}
-
-function handleNewCardSubmit(evt) {
-  evt.preventDefault();
-   renderLoading(true,formButtonCard);
-
-  const card = {
-    name: nameCardInput.value,
-    link: linkCardInput.value,
-  };
-
-  createNewCardSubmit(card)
-    .then((res) => {
-      const isMyLike = res.likes.filter((item) => item._id === user.id);
-      const isMyCard = res.owner._id === user.id;
-      elementsList.prepend(createCard(res, isMyLike.length > 0, isMyCard));
-      formCard.reset();
-      closePopup(popupAdd);
-    })
-    .catch((err) => {
-      console.log("Ошибка создания карточки", err.message);
-    })
-    .finally(() => {
-      renderLoading(false, formButtonCard);
-    });
-}
-
-formCard.addEventListener("submit", handleNewCardSubmit);
-
-formAvatar.addEventListener("submit", handleAvatarSubmit);
-
-formProfile.addEventListener("submit", handleProfileSubmit);
-
-formDelete.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  const element = document.querySelector(".element_active");
-
-  deleteCard(element)
-    .then(() => {
-      element.remove();
-
-      const popupOpened = document.querySelector(".popup_opened");
-      closePopup(popupOpened);
-    })
-    .catch((err) => {
-      console.log("Ошибка удаления карточки", err.message);
-    });
-});
